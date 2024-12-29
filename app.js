2d9eff64bb6ae0bf61997272fafcb89fa3186030
Narasingarao07 <tammineniramesh67@gmail.com>
2024-12-30 00:08:14 +0530

    last commit

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.get("/work", function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000.");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
